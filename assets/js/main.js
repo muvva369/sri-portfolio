b9fe3ecb9b5431b2150834a379d7e32c09bcfced
muvva369 <52378831+muvva369@users.noreply.github.com>
2020-06-10 12:26:52 +0530

    added avatar

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,134 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
+
+
+
+
+
+
+// previous
 (function($) {
+
+
+	$(function () {
+		$("#emailError").hide();
+		$("#subjectError").hide();
+		$("#form-submit-message").hide();
+		var error_email = false;
+	
+		//check email error function
+		function check_email() {
+		  var pattern = new RegExp(/^\b[A-Z0-9._%-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b$/i);
+		  if (pattern.test($("#email").val())) {
+			$("#emailError").hide();
+		  } else {
+			$("#emailError").html("Please enter a valid email id.");
+			$("#emailError").show();
+			error_email = true;
+		  }
+		}
+	
+		// check subject error fucntion
+		function check_subject() {
+		  if ($("#subject").val().length > 50) {
+			$("#subjectError").hide();
+			return false;
+		  }
+		  if (
+			$("#subject").focusout(function () {
+			  $("#subjectError").hide();
+			})
+		  );
+		  $("#subjectError").html(
+			"Remaining characters : " + (100 - $("#subject").val().length)
+		  );
+		  $("#subjectError").show();
+		}
+	
+		//clear form function
+		function clear_form() {
+		  $("#name").val("");
+		  $("#email").val("");
+		  $("#subject").val("");
+		  $("#message").val("");
+		}
+	
+		//control messages on focus
+		$("#name").focusin(function () {
+		  $("#form-submit-error").hide();
+		  $("#form-submit-message").hide();
+		});
+		$("#email").focusin(function () {
+		  $("#form-submit-error").hide();
+	
+		  $("#form-submit-message").hide();
+		});
+		$("#subject").focusin(function () {
+		  $("#form-submit-error").hide();
+		  $("#form-submit-message").hide();
+		});
+		$("#message").focusin(function () {
+		  $("#form-submit-error").hide();
+		  $("#form-submit-message").hide();
+		});
+	
+		$("#subject").keypress(function () {
+		  check_subject();
+		});
+	
+		$("#email").focusout(function () {
+		  check_email();
+		});
+	
+		//netlify fprm submission
+		$("#contact-form").submit(function (e) {
+		  e.preventDefault();
+		  error_email = false;
+	
+		  //validating email before submission
+		  check_email();
+	
+		  var emailInput = $("#email").val();
+		  var subjectInput = $("#subject").val();
+		  var textMsgInput = $("#message").val();
+		  var nameInput = $("#name").val();
+	
+		  if (
+			emailInput &&
+			subjectInput &&
+			textMsgInput &&
+			nameInput &&
+			error_email == false
+		  ) {
+			var $form = $(this);
+			$.post($form.attr("action"), $form.serialize()).then(function () {
+			  $("#form-submit-message").html(
+				"I have recevied your message! Thanks for writing."
+			  );
+			  $("#form-submit-message").show();
+			  clear_form();
+			});
+		  } else {
+			clear_form();
+			$("#form-submit-error").html(
+			  "Please fill in the details before submitting."
+			);
+			$("#form-submit-error").show();
+		  }
+		});
+	
+		$("#clear-form").click(function () {
+		  $("#name").val("");
+		  $("#email").val("");
+		  $("#subject").val("");
+		  $("#message").val("");
+		  $("#form-submit-error").hide();
+		  $("#form-submit-message").hide();
+		  $("#emailError").hide();
+		  $("#subjectError").hide();
+		});
+	  });
 
 	var $window = $(window),
 		$body = $('body'),
@@ -115,3 +242,159 @@
 			});
 
 })(jQuery);
+
+
+
+
+
+
+
+
+
+// (function ($) {
+// 	//contact form functions
+// 	$(function () {
+// 	  $("#emailError").hide();
+// 	  $("#subjectError").hide();
+// 	  $("#form-submit-message").hide();
+// 	  var error_email = false;
+  
+// 	  //check email error function
+// 	  function check_email() {
+// 		var pattern = new RegExp(/^\b[A-Z0-9._%-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b$/i);
+// 		if (pattern.test($("#email").val())) {
+// 		  $("#emailError").hide();
+// 		} else {
+// 		  $("#emailError").html("Please enter a valid email id.");
+// 		  $("#emailError").show();
+// 		  error_email = true;
+// 		}
+// 	  }
+  
+// 	  // check subject error fucntion
+// 	  function check_subject() {
+// 		if ($("#subject").val().length > 50) {
+// 		  $("#subjectError").hide();
+// 		  return false;
+// 		}
+// 		if (
+// 		  $("#subject").focusout(function () {
+// 			$("#subjectError").hide();
+// 		  })
+// 		);
+// 		$("#subjectError").html(
+// 		  "Remaining characters : " + (100 - $("#subject").val().length)
+// 		);
+// 		$("#subjectError").show();
+// 	  }
+  
+// 	  //clear form function
+// 	  function clear_form() {
+// 		$("#name").val("");
+// 		$("#email").val("");
+// 		$("#subject").val("");
+// 		$("#message").val("");
+// 	  }
+  
+// 	  //control messages on focus
+// 	  $("#name").focusin(function () {
+// 		$("#form-submit-error").hide();
+// 		$("#form-submit-message").hide();
+// 	  });
+// 	  $("#email").focusin(function () {
+// 		$("#form-submit-error").hide();
+  
+// 		$("#form-submit-message").hide();
+// 	  });
+// 	  $("#subject").focusin(function () {
+// 		$("#form-submit-error").hide();
+// 		$("#form-submit-message").hide();
+// 	  });
+// 	  $("#message").focusin(function () {
+// 		$("#form-submit-error").hide();
+// 		$("#form-submit-message").hide();
+// 	  });
+  
+// 	  $("#subject").keypress(function () {
+// 		check_subject();
+// 	  });
+  
+// 	  $("#email").focusout(function () {
+// 		check_email();
+// 	  });
+  
+// 	  //netlify fprm submission
+// 	  $("#contact-form").submit(function (e) {
+// 		e.preventDefault();
+// 		error_email = false;
+  
+// 		//validating email before submission
+// 		check_email();
+  
+// 		var emailInput = $("#email").val();
+// 		var subjectInput = $("#subject").val();
+// 		var textMsgInput = $("#message").val();
+// 		var nameInput = $("#name").val();
+  
+// 		if (
+// 		  emailInput &&
+// 		  subjectInput &&
+// 		  textMsgInput &&
+// 		  nameInput &&
+// 		  error_email == false
+// 		) {
+// 		  var $form = $(this);
+// 		  $.post($form.attr("action"), $form.serialize()).then(function () {
+// 			$("#form-submit-message").html(
+// 			  "I have recevied your message! Thanks for writing."
+// 			);
+// 			$("#form-submit-message").show();
+// 			clear_form();
+// 		  });
+// 		} else {
+// 		  clear_form();
+// 		  $("#form-submit-error").html(
+// 			"Please fill in the details before submitting."
+// 		  );
+// 		  $("#form-submit-error").show();
+// 		}
+// 	  });
+  
+// 	  $("#clear-form").click(function () {
+// 		$("#name").val("");
+// 		$("#email").val("");
+// 		$("#subject").val("");
+// 		$("#message").val("");
+// 		$("#form-submit-error").hide();
+// 		$("#form-submit-message").hide();
+// 		$("#emailError").hide();
+// 		$("#subjectError").hide();
+// 	  });
+// 	});
+// 	var $window = $(window),
+// 	  $body = $("body"),
+// 	  $nav = $("#nav");
+  
+// 	// Breakpoints.
+// 	breakpoints({
+// 	  xlarge: ["1281px", "1680px"],
+// 	  large: ["981px", "1280px"],
+// 	  medium: ["737px", "980px"],
+// 	  small: [null, "736px"],
+// 	});
+  
+// 	// Play initial animations on page load.
+// 	$window.on("load", function () {
+// 	  window.setTimeout(function () {
+// 		$body.removeClass("is-preload");
+// 	  }, 100);
+// 	});
+  
+// 	// Scrolly.
+// 	$("#nav a, .scrolly").scrolly({
+// 	  speed: 1000,
+// 	  offset: function () {
+// 		return $nav.height();
+// 	  },
+// 	});
+//   })(jQuery);
